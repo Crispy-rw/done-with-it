7e8a3d04f184564cd33416edf41876f1029c9626
Christian <nshimyumukizachristian>
2020-12-04 11:44:38 +0200

    Restructured forms Component

--- app/components/forms/ErrorMessage.js
+++ app/components/forms/ErrorMessage.js
@@ -1,6 +1,8 @@
 import React from "react";
-import AppText from "./AppText";
 import { StyleSheet } from "react-native";
+
+import AppText from "../AppText";
+
 function ErrorMessage({ error, visible }) {
   if (!visible || !error) return null;
   return <AppText style={styles.error}>{error}</AppText>;
